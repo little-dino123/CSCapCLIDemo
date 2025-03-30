5c373c86db29ef308f05c6bcc116573628aebd3e
davidZou <happydavid.zou@gmail.com>
2025-03-30 16:34:25 -0700

    updated example

--- example.py
+++ example.py
@@ -10,11 +10,14 @@ def cli():
 # sets up options and arguments
 @click.option("-t", "--times", default = 1)
 @click.option("-f", "--french", is_flag = True, default = False)
-@click.argument("name")
+@click.option("-c", "--cap",  is_flag = True, default = False)
+@click.argument("name", type = str)
 # the actual function of the command, the arguments passed in are automatically parsed by the library
-def hello(times, name, french):
+def hello(times, name, french, cap):
+    out = f"hello {name}" if not french else f"Bonjour, {name}!"
+    if cap: out = out.upper()
     for i in range(times):
-        click.echo(("hello", name) if not french else ("Bonjour, " + name+"!")) 
+        click.echo(out) 
         # echo is used  because it is more consistent than print, but it doesnt really matter here
 
 # this is just needed to make it work without packaging it
@@ -23,7 +26,7 @@ if __name__ == '__main__':
 
 """
 example usage: many ways to do the exact same thing
-python3 example.py hello -t 3 -f "Billy Bob Joe"
-python3 example.py hello "Billy Bob Joe" -t3 --french 
-python3 example.py hello -t3 "Billy Bob Joe" -f
+python3 example.py hello -t 3 -f "Billy Bob Joe" --cap
+python3 example.py hello "Billy Bob Joe" -t3 --french -c
+python3 example.py hello -t3 "Billy Bob Joe" -cf
 """
